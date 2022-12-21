fe05b899cb232f7830b4e190fc66a7366cbb4cff
olobaafeezco <adelajaafeezco@gmail.com>
2022-12-21 10:27:25 +0100

    progress

--- src/views/registration-form.vue
+++ src/views/registration-form.vue
@@ -12,32 +12,19 @@
                     </div>
                 </div>
             </div>
-            <div
-                class="bg-gradient-to-tr from-red-100 via-orange-500 to-red-500 py-20 md:py-32 backdrop-blur-md min-h-96">
-                <span class="absolute -z-10 w-full h-full inset-0 opacity-20"><img
-                        src="../assets/images/ass._class_cpt_g2_phlox.jpeg" alt=""
-                        class="w-full h-full object-cover -z-10"></span>
-                <span class="absolute -z-10 inset-0 bg-gradient-to-t from-indigo-500/80 to-purple-900/80 "></span>
-                <div class="max-w-6xl mx-auto max-sm:px-5 sm:px-16 md:px-0 z-50">
-                    <h3 class="max-md:text-3xl text-4xl text-white ">
-                        “Give a man a fish and you feed him for a day; teach a man to fish and you feed him for a
-                        lifetime.” — Maimonides
-                    </h3>
-                    <div class="btnGrp mt-8">
-                        <button class="text-indigo-500 rounded-full text-2xl bg-white py-4 px-10 shadow">Register
-                            Today</button>
-                    </div>
-                </div>
-           
         </div>
     </div>
 
-    <section>
-        <div class="w-8/12 mx-auto">
-            <h1>Kindly click the button below to download the form</h1>
-            <button>ADMISSION FORM</button>
-        </div>
-    </section>
+        <section>
+            <div class="text-center">
+                <h1 class="text-2xl font-bold">Kindly click the button below to download the form</h1>
+                <button>ADMISSION FORM</button>
+                <p>Or fill the form below</p>
+                <p>NB: *Please fill the name field as written on your Birth Certificate or International Passport.</p>
+            </div>
+
+        </section>
+
 
     <div class="bg-gradient-to-tr from-red-100 via-orange-500 to-red-500 py-20 md:py-32 backdrop-blur-md min-h-96">
         <span class="absolute -z-10 w-full h-full inset-0 opacity-20"><img
