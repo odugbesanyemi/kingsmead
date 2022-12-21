cb3b6d05f642e8fa578653bc7092e41033fecdbd
odugbesanyemi <odugbesanyemi@gmail.com>
2022-12-21 09:35:06 +0100

    space naming corrected

--- src/views/registration-form.vue
+++ src/views/registration-form.vue
@@ -31,7 +31,7 @@
             <div
                 class="bg-gradient-to-tr from-red-100 via-orange-500 to-red-500 py-20 md:py-32 backdrop-blur-md min-h-96">
                 <span class="absolute -z-10 w-full h-full inset-0 opacity-20"><img
-                        src="../assets/images/ass. class cpt g2 phlox.jpeg" alt=""
+                        src="../assets/images/ass._class_cpt_g2_phlox.jpeg" alt=""
                         class="w-full h-full object-cover -z-10"></span>
                 <span class="absolute -z-10 inset-0 bg-gradient-to-t from-indigo-500/80 to-purple-900/80 "></span>
                 <div class="max-w-6xl mx-auto max-sm:px-5 sm:px-16 md:px-0 z-50">
