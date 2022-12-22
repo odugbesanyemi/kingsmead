6ffd41c73dabc414f8e24438d2f07e2236d950fb
olobaafeezco <adelajaafeezco@gmail.com>
2022-12-22 23:32:28 +0100

    progress

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,13 @@
 
 <script>
 import { RouterView } from 'vue-router'
-import { createApp } from 'vue'
-import { Carousel, Slide } from 'vue-carousel'
-
 
 export default{
-  components:{
-  },
-  data(){
+   data(){
     return{
     }
   },
 }
+ 
+
 </script>
